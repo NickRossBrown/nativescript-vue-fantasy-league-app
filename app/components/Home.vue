d0710d3990b83f251a15d84a6baa25cf7b1e0d8a
Nick Brown <nickrossbrown@gmail.com>
2019-02-27 19:04:21 -0800

    init commit with imported css animations folder

---
+++ app/components/Home.vue
@@ -0,0 +1,47 @@
+<template>
+    <Page class="page">
+        <ActionBar class="action-bar">
+            <Label class="action-bar-title" text="Home"></Label>
+        </ActionBar>
+
+        <GridLayout>
+            <Label class="info" horizontalAlignment="center" verticalAlignment="center">
+                <FormattedString>
+                    <Span class="fa" text.decode="&#xf135; "/>
+                    <Span :text="message"/>
+                </FormattedString>
+            </Label>
+        </GridLayout>
+    </Page>
+</template>
+
+<script>
+    export default {
+        computed: {
+            message() {
+                return "Blank {N}-Vue app";
+            }
+        }
+    };
+</script>
+
+<style scoped lang="scss">
+    // Start custom common variables
+    @import '../app-variables';
+    // End custom common variables
+
+    // Custom styles
+    .fa {
+        color: $accent-dark;
+    }
+
+    .info {
+        font-size: 20;
+
+        opacity: 0;
+        animation-name: fadeInUp;
+        animation-duration: 0.5s;
+        animation-fill-mode: forwards;
+        animation-delay: 0.1s;
+    }
+</style>
